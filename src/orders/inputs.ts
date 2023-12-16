f34f3b12fa07c9446ae330d818c21e428dec5905
Efen Stakes <efenstakes101@gmail.com>
2023-12-16 13:43:49 +0300

    add get orders input

--- src/orders/inputs.ts
+++ src/orders/inputs.ts
@@ -1,7 +1,7 @@
 import { prop } from "@typegoose/typegoose";
 import mongoose from "mongoose";
 import { Field, InputType, } from "type-graphql";
-import { IsNotEmpty, IsPhoneNumber, MinLength } from "class-validator";
+import { IsMobilePhone, IsNotEmpty, IsPhoneNumber, Min, MinLength } from "class-validator";
 
 
 @InputType("PlaceOrderProductInput")
@@ -32,4 +32,67 @@ export class PlaceOrderInput {
     phone: string
 
     
+}
+
+
+@InputType("UpdateOrderInput")
+export class UpdateOrderInput {
+    
+    @IsNotEmpty()
+    @Field(()=> String, { })
+    id: string
+
+    @IsNotEmpty()
+    @Field(()=> String, { })
+    status: string
+
+    @IsMobilePhone()
+    @Field(()=> String, { })
+    phone: string
+
+    @Field(()=> Number, { })
+    deliveryDays?: number
+
+    @Field(()=> Number, {  })
+    shippingInitiatedOn?: number
+
+}
+
+
+@InputType("GetOrdersInput")
+export class GetOrdersInput {
+    
+    @IsNotEmpty()
+    @Field(()=> String, { })
+    status: string
+
+    @IsNotEmpty()
+    @Field(()=> String, { })
+    brand: string
+
+    @IsNotEmpty()
+    @Field(()=> String, { })
+    model: string
+
+    @IsMobilePhone()
+    @Field(()=> String, { })
+    phone: string
+
+    @Min(0)
+    @Field(()=> Number, { defaultValue: 0, })
+    offset?: number
+
+    @Min(30)
+    @Field(()=> Number, { defaultValue: 30, })
+    limit?: number
+
+
+    @Min(0)
+    @Field(()=> Number, { })
+    from?: number
+
+    @Min(0)
+    @Field(()=> Number, { })
+    to?: number
+
 }
